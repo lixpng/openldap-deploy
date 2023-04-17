402bd54f2adcc1916d41642db41542e29d4273dd
lixueping <lixueping@authing.cn>
2023-04-17 12:17:38 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+使用 docker-compose 部署 OpenLDAP，若需要启用 LDAPS，需要先执行 generate-cert.sh
---
+++ generate-cert.sh
@@ -0,0 +1,24 @@
+# /bin/sh
+# 注意填写 Common Name (e.g. server FQDN or YOUR name) [] 时要是 LDAP 访问域名
+# 临时目录
+mkdir .cert_tmp
+cd tmp
+# 创建根密钥
+openssl genrsa -out ldapca.key 2048
+# 创建自签名根证书
+openssl req -x509 -new -nodes -key ldapca.key -sha256 -days 1024 -out ldapca.pem
+# LDAP服务器创建私钥
+openssl genrsa -out ldap.key 2048
+# 创建证书签名请求
+openssl req -new -key ldap.key -out ldap.csr
+# 使用自定义根CA签署证书签名请求
+openssl x509 -req -in ldap.csr -CA ldapca.pem -CAkey ldapca.key -CAcreateserial -out ldap.crt -days 36600 -sha256
+# 拷贝
+cp ldap.{crt,key} ldapca.pem /workspace/coder/openldap-docker-compose/data/mount-in/certs/
+# 删除临时目录
+rm -rf .cert_tmp
+
+
+
+
+
